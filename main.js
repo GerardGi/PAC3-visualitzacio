5d5c49a0ffa997eaed6064350b2b174e7bc2510d
Gerard Ginés <gerardgi@uoc.edu>
2026-05-15 14:42:39 +0200

    Fix posició etiqueta mitjana

--- main.js
+++ main.js
@@ -126,7 +126,8 @@
           }},
           annotation: { annotations: { avg: {
             type:'line', xMin:37.5, xMax:37.5, borderColor:'#555555', borderWidth:1.5, borderDash:[6,4],
-            label:{ display:true, content:'Mitjana global 37,5%', position:'end',
+            label:{ display:true, content:'Mitjana global 37,5%',
+              position:{x:'center', y:'start'}, yAdjust:-10,
               backgroundColor:C.bg, color:'#555555', font:{size:11,weight:'bold'}, padding:4}
           }}},
         },
